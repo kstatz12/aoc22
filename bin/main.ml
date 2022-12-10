0e2b434aeb9be6749aefff27ee328381b261ec69
kstatz12 <karl.statz@gmail.com>
2022-12-10 16:28:05 -0600

    commiting

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,13 @@
-open Aoc22
+let read_lines name : string list =
+  let ic = open_in name in
+  let try_read () =
+    try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc = match try_read () with
+    | Some s -> loop (s :: acc)
+    | None -> close_in ic; List.rev acc in
+  loop []
+
+
 let () =
-  let lines = Common.read_lines "/home/karl.statz/aoc22/two.txt" in
-  List.iter print_string lines
+  let lines = read_lines "/home/karl.statz/aoc22/two.txt" in
+  Printf.printf "%d" (List.length lines)
--- lib/two.ml
+++ lib/two.ml
@@ -1,2 +1,44 @@
-let process list =
-  list
+let calculateScore pair =
+  match pair with
+  | ("A", "X") -> 4
+  | ("A", "Y") -> 8
+  | ("A", "Z") -> 3
+  | ("B", "X") -> 1
+  | ("B", "Y") -> 5
+  | ("B", "Z") -> 9
+  | ("C", "X") -> 7
+  | ("C", "Y") -> 2
+  | ("C", "Z") -> 6
+  | (_, _) -> 0
+
+
+let calculateScore2 pair =
+  match pair with
+  | ("A", "X") -> 3
+  | ("A", "Y") -> 4
+  | ("A", "Z") -> 8
+  | ("B", "X") -> 1
+  | ("B", "Y") -> 5
+  | ("B", "Z") -> 9
+  | ("C", "X") -> 2
+  | ("C", "Y") -> 6
+  | ("C", "Z") -> 7
+  | (_, _) -> 0
+
+let process list calcFn =
+
+  let to_tuples list =
+    let rec assign list ret = match list with
+      | [] -> ret
+      | h::t ->
+        let s = String.split_on_char ' ' h in
+        let n = (List.nth s 0, List.nth s 1)::ret in
+        assign t n in
+    assign list [] in
+
+  let rec proc list scores = match list with
+    | [] -> List.fold_left (+) 0 scores
+    | h::t ->
+      proc t ((calcFn h) :: scores) in
+
+  proc (to_tuples list) []
